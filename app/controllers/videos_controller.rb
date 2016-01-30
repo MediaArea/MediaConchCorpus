7fd7ab7957f2f69f39bd462acc1c259204a6aa7f
Ashley Blewer <ashley.blewer@gmail.com>
2016-01-30 16:45:41 -0500

    tidy params

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -43,15 +43,7 @@ class VideosController < ApplicationController
   private
 
   def video_params
-    params.require(:video).permit(
-      :id, :DocType, :DocTypeVersion, :DocTypeReadVersion, :Duration, :MuxingApp, :WritingApp, :DateUTC, :CRC32_Count, :Void_Count, 
-      :SeekHead_Count, :Info_Count, :Cluster_Count, :Tracks_Count, :Cues_Count, :Attachments_Count, :Chapters_Count, 
-      :Tags_Count, :CodecID_Video, :CodecID_Audio, :Format, :FileSize, :OverallBitRate, :URL, :EBML_ELEM_START, :EBML_VER_COH, 
-      :EBML_DOCVER_COH, :EBML_ELEMENT_VALID_PARENT, :EBML_ELEMENT_NONMULTIPLES, :EBML_ELEMENT_CONTAINS_MANDATES, 
-      :EBML_VALID_MAXID, :EBML_VALID_MAXSIZE, :ELEMENTS_WITHIN_MAXIDLENGTH, :ELEMENTS_WITHIN_MAXSIZELENGTH, 
-      :EBML_CRC_FIRST, :EBML_CRC_VALID, :EBML_CRC_LENGTH, :MKV_SEGMENT_UID_LENGTH, :MKV_VALID_TRACKTYPE_VALUE, 
-      :MKV_VALID_BOOLEANS, :MKV_SEEK_RESOLVE, :created_at, :updated_at
-      )
+    params.require(:video).permit(facet_arr.each{|str| str})
   end
 
 end
